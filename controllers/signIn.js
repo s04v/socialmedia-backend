3102ffac793f07a3186070ca8c79e825231304ff
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-01-14 14:31:50 +0100

    Change response in signIn service

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -6,13 +6,13 @@ const signIn = async (req, res) => {
     const { error } =  SignInSchema.validate(req.body);
     if(error) {
         console.log(error.details[0].message);
-        res.status(400).send(createResponse(error));
+        res.send(createResponse("error", null, error.details[0].message));
         return;
     }
 
     const param = req.body;
-    const str = await SignInService(param.login, param.password);
-    res.send(str);
+    const {status, data, msg} = await SignInService(param.login, param.password);
+    res.send(createResponse(status, data, msg));
 }
 
 module.exports = signIn;
--- services/signIn.js
+++ services/signIn.js
@@ -7,12 +7,12 @@ const signIn = async (login, password) => {
     const data = await userModel.findOne({where:{login:login, password:cryptPassword(password)}})
         .catch( e => console.log(e));
     if(data <= 0)
-        return "invalid login or password";
+        return { status: "error", msg: "Invalid login or password" };
 
     const user = data.dataValues;
     const sign = jwt.sign({id: user.id, login: user.login}, process.env.SECRET_TOKEN, { expiresIn:86400});
 
-    return { jwt: sign };
+    return { status: "success", data: { jwt: sign } };
 }
 
 module.exports = signIn;
